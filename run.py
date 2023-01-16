c39f09c3fa611f84c087ae7b7486a21c08a95856
yinwwwhao <yinwwwhao@outlook.com>
2023-01-16 18:00:38 +0800

    listen 0.0.0.0:8000

--- run.py
+++ run.py
@@ -7,6 +7,6 @@ from configs import mode
 
 if __name__ == '__main__':
     if mode == 'dev':
-        app.run(debug=True)
+        app.run(host='0.0.0.0', port=8000, debug=True)
     else:
         serve(app, host='localhost', port=8000)
